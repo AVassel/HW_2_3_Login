c80dad9a52e82777dbb77a595b001a9bbb39726c
Anton Vassel <ant-vassel@ya.ru>
2022-04-01 15:43:43 +0300

    some update

--- HW_2_3_Login/ViewController.swift
+++ HW_2_3_Login/ViewController.swift
@@ -14,8 +14,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        loginTF.delegate = self
-//        passwordTF.delegate = self
         
     }
 
